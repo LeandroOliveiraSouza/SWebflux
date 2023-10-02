34dfdda276b2b9f9618e4f1019c58c3841ef4cfa
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-10-02 18:13:55 -0300

    @DocumentReference

--- spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/config/SeedingDatabase.java
+++ spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/config/SeedingDatabase.java
@@ -49,10 +49,5 @@ public class SeedingDatabase implements CommandLineRunner {
 
 		Flux<Post> insertPosts = postRepository.saveAll(Arrays.asList(post1, post2));
 		insertPosts.subscribe();
-
-		/*
-		maria.getPosts().addAll(Arrays.asList(post1, post2));
-		userRepository.save(maria);
-		 */
 	}
 }
